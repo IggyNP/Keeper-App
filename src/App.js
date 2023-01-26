5bb9357827017d102e669fab3830576bcdfb6ffb
Iggynp <odissey0x@gmail.com>
2023-01-26 13:16:41 +0100

    added mapping

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Note from "./Components/Note";
+import notes from "./notes";
 
 const App = () => {
-	return (
-		<div>
-			<Header />
-			<Note />
-			<Footer />
-		</div>
-	);
-}
+  return (
+    <div>
+      <Header />
+      {notes.map((noteEntry) => (
+        <Note
+          key={noteEntry.key}
+          title={noteEntry.title}
+          content={noteEntry.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+};
 export default App;
-
